b0697c265fa7a16ff0d832aa0bf83d57032ac7f0
Chaitanya Kelkar <chaitanyaakelkar57@gmail.com>
2025-04-03 10:19:49 +0530

    Time: 293 ms (83.98%), Space: 0B (100%) - LeetHub

---
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -0,0 +1,2 @@
+# Write your MySQL query statement below
+select (select salary from Employee where salary < (select max(salary) from Employee) order by salary desc limit 1) as "SecondHighestSalary"
