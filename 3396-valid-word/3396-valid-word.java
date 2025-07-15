de56fa3fb7dc668ddd27dc3e5656054f8402fec4
Chaitanya Kelkar <chaitanyaakelkar57@gmail.com>
2025-07-15 10:30:55 +0530

    Time: 1 ms (99.12%), Space: 42.3 MB (34.66%) - LeetHub

--- 3396-valid-word/3396-valid-word.java
+++ 3396-valid-word/3396-valid-word.java
@@ -4,15 +4,12 @@ class Solution {
 
         boolean hasVowel = false;
         boolean hasConsonant = false;
-        HashSet<Character> vowels = new HashSet<>();
-        for (Character i: "aeiou".toCharArray()){
-            vowels.add(i);
-        }
         word = word.toLowerCase();
         for (int i = 0; i < word.length(); i++){
             if (!Character.isLetterOrDigit(word.charAt(i))) return false;
             if (Character.isLetter(word.charAt(i))){
-                if (vowels.contains(word.charAt(i))){
+                char j = word.charAt(i);
+                if (j == 'a' || j == 'e' || j == 'i' || j == 'o' || j == 'u'){
                     hasVowel = true;
                 } else {
                     hasConsonant = true;
