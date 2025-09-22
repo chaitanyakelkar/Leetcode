c3921c848a0c77a86e4230e1248c6db523fed2fa
Chaitanya Kelkar <chaitanyaakelkar57@gmail.com>
2025-09-22 11:38:17 +0530

    Time: 2 ms (98.74%), Space: 44.9 MB (75.25%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
@@ -2,25 +2,24 @@ class Solution {
     public String reverseVowels(String s) {
         int left = 0, right = s.length() - 1;
         char temp;
-
-        StringBuilder str = new StringBuilder(s);
+        char[] str = s.toCharArray();
+        
+        boolean[] vowels = new boolean[128];
+        for (char c: "aeiouAEIOU".toCharArray()){
+            vowels[c] = true;
+        }
 
         while (left < right){
-            while (!isVowel(s.charAt(left)) && left < right) left++;
-            while (!isVowel(s.charAt(right)) && left < right) right--;
+            while (!vowels[str[left]] && left < right) left++;
+            while (!vowels[str[right]] && left < right) right--;
 
-            temp = str.charAt(left);
-            str.replace(left, left+1, Character.toString(str.charAt(right)));
-            str.replace(right, right+1, Character.toString(temp));
+            temp = str[left];
+            str[left] = str[right];
+            str[right] = temp;
             left++;
             right--;
         }
 
-        return str.toString();
-    }
-    public boolean isVowel(char c){
-        if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U')
-            return true;
-        return false;
+        return new String(str);
     }
 }
