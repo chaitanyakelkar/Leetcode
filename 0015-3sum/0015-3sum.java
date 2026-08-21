38d6e6ae18f44d10f7e9e76e557e448f3f72b896
Chaitanya Kelkar <chaitanyaakelkar57@gmail.com>
2026-08-21 14:40:05 +0530

    Time: 33 ms (72.24%), Space: 59.3 MB (45.48%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,33 +1,36 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> res = new ArrayList<>();
+        List<List<Integer>> result = new ArrayList<>();
         Arrays.sort(nums);
 
-        for (int i = 0; i < nums.length - 2; i++){
-            if (i > 0 && nums[i] == nums[i-1]) continue;
+        for (int i = 0; i < nums.length; i++) {
+            // Skip duplicate values for the first element of the triplet.
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
 
             int left = i + 1;
             int right = nums.length - 1;
 
-            while (left < right){
+            while (left < right) {
                 int sum = nums[i] + nums[left] + nums[right];
 
-                if (sum == 0){
-                    res.add(Arrays.asList(nums[i], nums[left], nums[right]));
-
-                    while (left < right && nums[left] == nums[left + 1]) left++;
-                    while (left < right && nums[right] == nums[right - 1]) right--;
-
-                    left++;
+                if (sum > 0) {
                     right--;
-                } else if (sum < 0){
+                } else if (sum < 0) {
                     left++;
                 } else {
+                    result.add(new ArrayList<>(List.of(nums[i], nums[left], nums[right])));
+
+                    // Move both pointers past the values just used.
+                    left++;
                     right--;
+
+                    // Skip duplicates for left and right.
+                    while (left < right && nums[left] == nums[left - 1]) left++;
+                    while (left < right && nums[right] == nums[right + 1]) right--;
                 }
             }
         }
 
-        return res;
+        return result;
     }
 }
