ed52996b461a5b6f6015cc23610bfb033872b4a0
Chaitanya Kelkar <chaitanyaakelkar57@gmail.com>
2026-08-27 20:08:44 +0530

    Time: 0 ms (100%), Space: 44.1 MB (8.73%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,22 +1,28 @@
 class Solution {
+    public boolean binarySearch(int[][] matrix, int rowIdx, int target){
+        int left = 0;
+        int right = matrix[rowIdx].length - 1;
+        int mid;
+        while (left <= right){
+            mid = (left + right) / 2;
+
+            if (matrix[rowIdx][mid] == target) return true;
+            else if (matrix[rowIdx][mid] > target) right = mid - 1;
+            else if (matrix[rowIdx][mid] < target) left = mid + 1;
+        }
+        return false;
+    }
+
     public boolean searchMatrix(int[][] matrix, int target) {
-        int low = 0;
-        int high = matrix.length * matrix[0].length - 1;
-        int mid = 0;
-        int midval = 0;
-        int m = matrix[0].length;
+        int left = 0;
+        int right = matrix.length - 1;
+        int mid;
+        while (left <= right){
+            mid = (left + right) / 2;
 
-        while (low <= high){
-            mid = (low + high) / 2;
-
-            midval = matrix[mid / m][mid % m];
-            if (midval == target){
-                return true;
-            } else if (target > midval){
-                low = mid + 1;
-            } else {
-                high = mid - 1;
-            }
+            if (matrix[mid][0] <= target && matrix[mid][matrix[mid].length-1] >= target) return binarySearch(matrix, mid, target);
+            else if (matrix[mid][0] > target) right = mid - 1;
+            else if (matrix[mid][matrix[mid].length-1] < target) left = mid + 1;
         }
 
         return false;
