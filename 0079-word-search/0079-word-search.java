5d956bc91937e2c3d9ceda06a39874f1b77d6609
Chaitanya Kelkar <chaitanyaakelkar57@gmail.com>
2026-09-04 16:55:14 +0530

    Time: 138 ms (49.16%), Space: 42.9 MB (75.8%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,13 +1,13 @@
 class Solution {
-    private boolean dfs(char[][] board, boolean[][] visited, StringBuilder word, int i, int j){
+    private boolean dfs(char[][] board, boolean[][] visited, String word, int index, int i, int j){
         if (i < 0 || i >= board.length) return false;
         if (j < 0 || j >= board[i].length) return false;
         if (visited[i][j]) return false;
-        if (word.charAt(0) != board[i][j]) return false;
+        if (word.charAt(index) != board[i][j]) return false;
         visited[i][j] = true;
-        word.deleteCharAt(0);
-        if (word.length() == 0) return true;
-        boolean found =  dfs(board, visited, new StringBuilder(word), i, j-1) || dfs(board, visited, new StringBuilder(word), i, j+1) || dfs(board, visited, new StringBuilder(word), i-1, j) || dfs(board, visited, new StringBuilder(word), i+1, j);
+        index++;
+        if (index == word.length()) return true;
+        boolean found =  dfs(board, visited, word, index, i, j-1) || dfs(board, visited, word, index, i, j+1) || dfs(board, visited, word, index, i-1, j) || dfs(board, visited, word, index, i+1, j);
         visited[i][j] = false;
         return found;
     }
@@ -16,8 +16,7 @@ class Solution {
         for(int i = 0; i < board.length; i++){
             for (int j = 0; j < board[i].length; j++){
                 boolean[][] visited = new boolean[board.length][board[0].length];
-                StringBuilder sb = new StringBuilder(word);
-                if (dfs(board, visited, sb, i, j)) return true;
+                if (dfs(board, visited, word, 0, i, j)) return true;
             }
         }
         return false;
